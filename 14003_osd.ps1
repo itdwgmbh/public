6e9837ceb5615dca2ddc6679a254c1bfe28c8835
Daniel Waldmann <d.waldmann@it-dw.com>
2023-08-02 07:28:24 +0000

    Add 14003_osd.ps1

---
+++ 14003_osd.ps1
@@ -0,0 +1,14 @@
+Import-Module OSDN -Force
+
+# Customize GUI
+$OSDModuleResource.StartOSDCloudGUI.BrandName = 'EIfER'
+$OSDModuleResource.StartOSDCloudGUI.updateFirmware = $true
+$OSDModuleResource.StartOSDCloudGUI.ClearDiskConfirm = $false
+
+# Customize OS Choice
+$OSDModuleResource.OSDCloud.Values.Language = @('en-us','de-de','fr-fr')
+$OSDModuleResource.OSDCloud.Values.ReleaseID = '22H2'
+$OSDModuleResource.OSDCloud.Values.Activation = 'Volume'
+
+# Start Deployment
+Start-OSDCloudGui
